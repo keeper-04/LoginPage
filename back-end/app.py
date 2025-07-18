6961209952ce043775a3dd002d1c574b3e591c31
mnikol08 <mnikol08@ucy.ac.cy>
2025-07-18 12:28:50 +0300

    users initialization

--- back-end/app.py
+++ back-end/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import json
 import os
 
 app = Flask(__name__)
+CORS(app)  # ← Add this line
 USER_FILE = 'users.json'
 
 def load_users():
